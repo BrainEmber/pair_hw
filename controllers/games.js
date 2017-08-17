ecd7a1c86a44641f01f1b8424b473d90eb17cf9b
Jellis <venetianself@hotmail.com>
2017-08-17 14:19:55 -0500

    Basic routes in controller built

--- controllers/games.js
+++ controllers/games.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const Games = require('../models/games.js');
 
 router.get('/', (req, res)=>{
-	res.send('super sweet');
+	res.send('index route');
 });
+
+router.post('/', (req, res)=>{
+	res.send('post route');
+});
+
+router.delete('/', (req, res)=>{
+	res.send('delete route');
+});
+
+router.put('/:id', (req, res)=>{
+	res.send('put route');
+});
+
 module.exports = router;
